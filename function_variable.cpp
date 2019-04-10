d27c2286d64868a21a03ec080244aa0d5fa3ddcd
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-10 15:37:05 +0300

    Adding the git ignore of our apps

---
+++ function_variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// sum of number in one  function in cpp
+void func(int x, int y){
+ 	cout << "sum of " << x << " and " << y << " is " << x + y << endl;
+ }
+
+void power(int x) {
+
+}
+int main()
+ {
+ 	/* code */
+ 	func(6, 5);
+ 	return 0;
+ } 
+
