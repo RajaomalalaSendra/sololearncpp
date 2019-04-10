e5d408a9d494bf97b296b1aad56ec7008c34e342
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-10 16:06:11 +0300

    This is the creation of the app as the function variable in cpp

--- function_variable.cpp
+++ function_variable.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 // sum of number in one  function in cpp
@@ -7,13 +8,15 @@ void func(int x, int y){
  	cout << "sum of " << x << " and " << y << " is " << x + y << endl;
  }
 
-void power(int x) {
-
+int power(int x) {
+    return x * 2;
 }
 int main()
  {
  	/* code */
  	func(6, 5);
+ 	// The power of 2
+ 	cout << "The number is: " << power(2);
  	return 0;
  } 
 
