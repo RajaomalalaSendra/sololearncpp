f90e19431e3b3dfba2ba69b586b46fbf622baecc
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-10 15:08:44 +0300

    This is the creation of the function prototype

--- function_in_cpp.cpp
+++ function_in_cpp.cpp
@@ -5,19 +5,31 @@ using namespace std;
 void myFirstFunction(){
 	cout << "I am using here the first function in cpp" << endl; 
 }
+// This is the function prototype in cpp
+void calculusList();
+
+// The main function in cpp
 int main() {
 	/*
     This is the call of the function that we have just created in cpp above
 	*/
 	string *function = new string;
-	cout << "just type 'function'" << endl;
+	cout << "just type 'function' or 'calculus'" << endl;
 	cout << "> ";
 	cin >> *function;
 	if (*function == "function") {
 		cout << "Congratulation" << endl;
 		myFirstFunction();
+	} else if (*function == "calculus"){
+		cout << "Congrats!! You have just created the list of calculus!" << endl;
+		calculusList();
 	} else {
 		cout << "Sorry for the bad interuption!! See you next time.";
 	}
+	delete function;
 	return 0;
+}
+
+void calculusList(){
+	cout << "Here are the list of the calculus:\nAddition\tsubstraction\tDivision\tDivision" << endl; 
 }
