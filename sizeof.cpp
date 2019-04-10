cb791b44217bde3a230f60c451348ca17f261f7c
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-10 13:13:20 +0300

    this is the cpp usizing the size inside of all of it

---
+++ sizeof.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	/*
+    This is the program we gonna learn for today concren about the size of in cpp
+	*/
+	cout << "char: " << sizeof(char) << endl;
+    cout << "int: " << sizeof(int) << endl;
+    cout << "float: " << sizeof(float) << endl;
+    cout << "double: " << sizeof(double) << endl;
+    int var = 50;
+    cout << "var: " << sizeof(var) << endl;
+    int *size = new int[20];
+    cout << "I want to know the size of the number: " << endl;
+    cout << "> ";
+    cin >> *size;
+    cout << "It's lenght is: " << sizeof(*size) << endl;
+    delete size;
+	return 0;
+}
