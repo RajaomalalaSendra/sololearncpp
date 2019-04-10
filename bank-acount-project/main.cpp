9ad6cb38865755adda4a26ef9f6fed4d65531142
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-10 23:01:30 +0300

    This is the modification for the last time in cpp of creating the bank account

--- bank-acount-project/main.cpp
+++ bank-acount-project/main.cpp
@@ -46,33 +46,122 @@ void account::read_the_data() {
 	cin >> adress;
 	cout << "Enter The Email: " << endl;
 	arrow();
-	cin >> firstName;
+	cin >> email;
     cout << "Enter The Balance: " << endl;
     arrow();
     cin >> totalBalance;
     cout << endl;
 }
 void account::show_the_data() {
-	cout << "this is the showing  ofthe data" << endl;
+	cout << "Account Number: " << account_number << endl;
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Adress: " << adress << endl;
+    cout << "Email: " << email << endl;
+    cout << "Current Balance: Rs.  " << totalBalance << endl;
+    cout << "-------------------------------" << endl;
 }
 void account::write_the_record() {
     ofstream outfile;
-    outfile.open("record.bank", ios::binary|ios::app);
+    outfile.open("record.data", ios::binary|ios::app);
     read_the_data();
-    outfile.write(reinterpret_cast<char *>(this), sizeof(*this));
+    outfile.write(reinterpret_cast<char*>(this), sizeof(*this));
     outfile.close();
 }
 void account::read_the_record() {
-	cout << "this is the reading of the record in cpp." << endl;
+	ifstream infile;
+    infile.open("record.data", ios::binary);
+    if(!infile)
+    {
+        cout<<"Error in Opening! File Not Found!!"<<endl;
+        return;
+    }
+    cout<<"\n****Data from The file is here****"<<endl;
+    while(!infile.eof())
+    {
+        if(infile.read(reinterpret_cast<char*>(this), sizeof(*this)))
+        {
+            show_the_data();
+        }
+    }
+    infile.close();
 }
 void account::search_the_record() {
-	cout << "this is the searching of the record in cpp." << endl;
+	int n;
+    ifstream infile;
+    infile.open("record.data", ios::binary);
+    if(!infile)
+    {
+        cout << "\nError in opening! File Not Found!!" << endl;
+        return;
+    }
+    infile.seekg(0,ios::end);
+    int count = infile.tellg()/sizeof(*this);
+    cout << "******" << endl;
+    ios::end;
+    cout << "The infile.tellg: " << infile.tellg() << endl;
+    cout << "The size of this: " << sizeof(*this) << endl;
+    cout << "******" << endl;
+    cout << "\n There are " << count << " record in the file";
+    cout << "\n Enter Record Number to Search: ";
+    cin >> n;
+    infile.seekg((n-1)*sizeof(*this));
+    infile.read(reinterpret_cast<char*>(this), sizeof(*this));
+    show_the_data();
 }
 void account::edit_the_record() {
-	cout << "this is the editing of  the record in cpp." << endl; 
+	int n;
+    fstream iofile;
+    iofile.open("record.data", ios::in|ios::binary);
+    if(!iofile)
+    {
+        cout<<"\nError in opening! File Not Found!!"<<endl;
+        return;
+    }
+    iofile.seekg(0, ios::end);
+    int count = iofile.tellg()/sizeof(*this);
+    cout<<"\n There are "<<count<<" record in the file";
+    cout<<"\n Enter Record Number to edit: ";
+    cin>>n;
+    iofile.seekg((n-1)*sizeof(*this));
+    iofile.read(reinterpret_cast<char*>(this), sizeof(*this));
+    cout<<"Record "<<n<<" has following data"<<endl;
+    show_the_data();
+    iofile.close();
+    iofile.open("record.data", ios::out|ios::in|ios::binary);
+    iofile.seekp((n-1)*sizeof(*this));
+    cout<<"\nEnter data to Modify "<<endl;
+    read_the_data();
+    iofile.write(reinterpret_cast<char*>(this), sizeof(*this)); 
 }
 void account::delete_the_record() {
-	cout << "this is the deleting of the record in cpp." << endl;
+	int n;
+    ifstream infile;
+    infile.open("record.data", ios::binary);
+    if(!infile)
+    {
+        cout<<"\nError in opening! File Not Found!!"<<endl;
+        return;
+    }
+    infile.seekg(0,ios::end);
+    int count = infile.tellg()/sizeof(*this);
+    cout<<"\n There are "<<count<<" record in the file";
+    cout<<"\n Enter Record Number to Delete: ";
+    cin>>n;
+    fstream tmpfile;
+    tmpfile.open("tmpfile.data", ios::out|ios::binary);
+    infile.seekg(0);
+    for(int i=0; i<count; i++)
+    {
+        infile.read(reinterpret_cast<char*>(this),sizeof(*this));
+        if(i==(n-1))
+            continue;
+        tmpfile.write(reinterpret_cast<char*>(this), sizeof(*this));
+    }
+    infile.close();
+    tmpfile.close();
+    remove("record.data");
+    rename("tmpfile.data", "record.data");
 }
 int main() {
 	/*
@@ -81,6 +170,16 @@ int main() {
 	account Account;
 	int *choose = new int;
     
+    // renive the record.data if it exists
+    ifstream deletefile;
+    deletefile.open("record.data", ios::binary);
+    if(deletefile)
+    {
+         remove("record.data");
+    }
+    deletefile.close();
+
+
     // this is the enter to the account information
 	cout << "** Get Access to the information System in Our bank **" << endl;
 	while (true) {
