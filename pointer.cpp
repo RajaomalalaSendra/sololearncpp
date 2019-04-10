2fb1dddca024740fb37c438a07abf04e5f34b8d8
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-10 09:24:24 +0300

    this is the modification of the pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -8,16 +8,30 @@ int main()
     Declaration of the variable
 	*/
 	unsigned int a[5] = {1,2,4,7,7};
-	int b; 
+	int score[2][3] = {{1,2,2},{1,3,2}};
+	int b, e, f; 
+	int *mypointer;
 	cout << "Using the pointer in cpp" << endl;
 	cout << "first of all we need to get your answer" << endl;
 	cout << "enter a number between 0 and 4" << endl;
 	cin >> b;
 	if (b < 5 && b >= 0) {
 		cout << "the answer of that number is: " << a[b] << endl;
+		cout << "The pointer of b is: " << &b << "\tThe pointer of a is: " << &a << endl;
 	} else {
 		cout << "That is a wrong aswer!! So sorry for that." << endl;
+		cout << "The pointer of b is: " << &b << endl;
 	}
-	
+	cout << "Enter the row that is between 0 or 1: " << endl;
+    cin >> e;
+    cout << "Enter the column of that which is between 0 and 2: " << endl;
+    cin >> f;
+    if (e == 0 || e == 1 && f >= 0 && f < 3 ) {
+        cout << "Your answer is: " << score[e][f] << endl;
+        cout << "It's pointer is: "<< &score[e][f] << endl;	
+    } else {
+    	cout << "That is a wrong answer for all of that we expected!!" << endl;
+    }
+	cout << "<mypointer>: " << mypointer << endl;
 	return 0;
 }
