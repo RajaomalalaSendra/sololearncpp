d740fdd5f7795b9633a03c89b110a3eaf756fd10
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-11 08:36:15 +0300

    This is the project of todolist in cpp

---
+++ todolist/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// the main function
+int main()
+{
+	/* 
+    This is the main function of this project
+	 */
+	cout << "The main project of todolist in cpp." << endl;
+	return 0;
+}
