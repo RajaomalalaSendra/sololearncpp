b27a74a84c5e19a7a5ef93c95c7abdb939e1aa1d
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-11 14:58:18 +0300

    creation of the another class with the ...

--- encapsulation_cpp.cpp
+++ encapsulation_cpp.cpp
@@ -8,10 +8,28 @@ class myClass {
     string surname;
 };
 
+class myAnotherClass {
+  public:
+    void setName(string x) {
+      name = x;
+    }
+    string getName() {
+    	return name;
+    }
+  private:
+    string name;
+};
+
+
 int main() {
+  // creation of the object
   myClass myObj;
+  myAnotherClass myAnotherObj;
+  // the variable inside the object
+  myAnotherObj.setName("Sendra");
   myObj.name = "SoloLearn";
   myObj.surname = "The CPP";
   cout << myObj.name << " " << myObj.surname << endl;
+  cout << myAnotherObj.getName() << endl;
   return 0;
 }
