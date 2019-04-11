537efd3a30e83669bad17aaee142b7bf49dc411f
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-11 13:50:33 +0300

    update the pass_by_reference.cpp

--- pass_by_reference.cpp
+++ pass_by_reference.cpp
@@ -5,10 +5,16 @@ using namespace std;
 void myFunc(int x) {
   x = 100;
 }
-
+void myFunction(int *y){
+	*y = 100;
+}
 int main() {
   int var = 20;
-  myFunc(var);
+  int myvar = 30;
+  cout << "Pass by value" << endl;
+  myFunc(myvar);
+  cout << "Pass by reference" << endl;
+  myFunction(&var);
   cout << var;
 }
 // Outputs 20
