fc935f8ecc47edddb132883f5e1ffbcf25335370
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-04-11 12:53:52 +0300

    the array and function in cpp

---
+++ Array_and_function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// The first array and function in cpp
+void printArray(int arr[], int size) {
+  for(int x=0; x<size; x++) {
+    cout << "You know this: " << arr[x];
+  }
+}
+
+int main()
+{
+	/* code */
+	int myArr[3]= {42, 33, 88};
+    printArray(myArr, 3);
+	return 0;
+}
